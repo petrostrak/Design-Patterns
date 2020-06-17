4875ebc72d837d7b538b4d5e974a196e09180eb8
petros <pit.trak@gmail.com>
2020-06-17 18:34:52 +0300

    Giving a whirl at factory main class

--- Creational Design Patterns/Factory/Example #1/src/model/Main.java
+++ Creational Design Patterns/Factory/Example #1/src/model/Main.java
@@ -1,9 +1,20 @@
 package model;
 
+import interfaces.HamburgerStore;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		HamburgerStore mozambicanBurgerStore = new MozHamburgerStore();
+		
+		HamburgerStore jamaicanBurgerStore = new JamHamburgerStore();
+		
+		Hamburger hamburger = mozambicanBurgerStore.orderHamburger("cheese");
+		System.out.println("Peter ordered " + hamburger.getName());
+		
+		hamburger = jamaicanBurgerStore.orderHamburger("veggie");
+		System.out.println("Maggie ordered an " + hamburger.getName());
 	}
 
 }
