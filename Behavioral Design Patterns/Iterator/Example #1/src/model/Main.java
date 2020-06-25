06166981dbfcfe0b5831ed1b50ec6b60981bb7a0
petros <pit.trak@gmail.com>
2020-06-25 20:23:31 +0300

    added another iterator class

--- Behavioral Design Patterns/Iterator/Example #1/src/model/Main.java
+++ Behavioral Design Patterns/Iterator/Example #1/src/model/Main.java
@@ -7,26 +7,29 @@ public class Main {
 	public static void main(String[] args) {
 		
 		DevStoreCatalog devStoreCatalog = new DevStoreCatalog();
-		Product[] devCatalog = devStoreCatalog.getCatalog();
+		//Product[] devCatalog = devStoreCatalog.getCatalog();
 		
 		GeekyStoreCatalog geekyStoreCatalog = new GeekyStoreCatalog();
-		ArrayList geekyCatalog = geekyStoreCatalog.getCatalog();
+		//ArrayList<Product> geekyCatalog = geekyStoreCatalog.getCatalog();
 		
-		//loop through the Dev catalog array
-		for(int i = 0; i < devCatalog.length; i ++) {
-			Product product = devCatalog[i];
-			System.out.print(product.getName() + " ");
-			System.out.print(product.getDescription() + " ");
-			System.out.println(product.getPrice());
-		}
+		Seller seller = new Seller(geekyStoreCatalog);
+		seller.printCatalog();
 		
-		//loop through the Geeky catalog list
-		for(int i = 0; i < geekyCatalog.size(); i ++) {
-			Product product = (Product) geekyCatalog.get(i);
-			System.out.print(product.getName() + " ");
-			System.out.print(product.getDescription() + " ");
-			System.out.println(product.getPrice());
-		}
+//		//loop through the Dev catalog array
+//		for(int i = 0; i < devCatalog.length; i ++) {
+//			Product product = devCatalog[i];
+//			System.out.print(product.getName() + " ");
+//			System.out.print(product.getDescription() + " ");
+//			System.out.println(product.getPrice());
+//		}
+//		
+//		//loop through the Geeky catalog list
+//		for(int i = 0; i < geekyCatalog.size(); i ++) {
+//			Product product = (Product) geekyCatalog.get(i);
+//			System.out.print(product.getName() + " ");
+//			System.out.print(product.getDescription() + " ");
+//			System.out.println(product.getPrice());
+//		}
 		
 	}
 
--- Behavioral Design Patterns/Iterator/Example #1/src/model/Seller.java
+++ Behavioral Design Patterns/Iterator/Example #1/src/model/Seller.java
@@ -1,5 +1,27 @@
 package model;
 
-public class Seller  {
+import interfaces.GeekyStoreIterator;
+import interfaces.Iterator;
 
+public class Seller  {
+	GeekyStoreCatalog geekyStoreCatalog;
+
+	public Seller(GeekyStoreCatalog geekyStoreCatalog) {
+		this.geekyStoreCatalog = geekyStoreCatalog;
+	}
+	
+	public void printCatalog() {
+		GeekyStoreIterator geekyStoreIterator = geekyStoreCatalog.createIterator();
+		System.out.println("Printing catalog: ");
+		printCatalog(geekyStoreIterator);
+	}
+	
+	private void printCatalog(GeekyStoreIterator geekyStoreIterator) {
+		while(geekyStoreIterator.hasNext()) {
+			Product product = (Product)geekyStoreIterator.next();
+			System.out.print(product.getName() + ", ");
+			System.out.print(product.getDescription() + " ");
+			System.out.println(product.getPrice());
+		}
+	}
 }
